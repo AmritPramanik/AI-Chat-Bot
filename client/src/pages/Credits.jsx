4ad9861d32dbb773e2d120069073b00e852da852
Amrit Pramanik <pramanikamrit778@gmail.com>
2025-08-25 21:36:38 +0530

    Initial Commit

---
+++ client/src/pages/Credits.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Credits = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Credits
+
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      
+      L
     </div>
   )
 }
 
 export default Login
+
